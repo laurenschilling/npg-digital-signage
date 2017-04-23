95cb6b0051f9c4f90d660578a3bde81b169cd774
caitlinmaree <caitlin.willington@hotmail.com>
2017-04-23 18:33:07 +1000

    Slider + Content
    Updated slider to work with javascript on a automated timer, with
    corresponding CSS and HTML. Content updated to screen one also.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,23 @@
 ///////////////////////////////////////////////////////
+// Page 1 "Image slider"
+///////////////////////////////////////////////////////
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+///////////////////////////////////////////////////////
 // Page 2 "Pick a Portrait"
 ///////////////////////////////////////////////////////
 
