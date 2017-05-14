6bfb678ac057f607097452a93f9f8688e55a7cb3
bonney6 <bonney.rowe@live.com>
2017-05-15 00:53:44 +1000

    ProTip/help button + gallery slider
    1. Put Gallery back in pocket and hid the scroll - gallery is the same
    width as logo
    2. Added Protip button - Js + Css + html changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -320,3 +320,27 @@ function saveImage() {
 	  // Once it has been saved then you can go off and do the other things to it
 	});
 };
+
+
+//help button WIP bonney
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function giveHelp() {
+    document.getElementById("myHelpbtn").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.helpbtn')) {
+
+    var helpbuttons = document.getElementsByClassName("help-content");
+    var i;
+    for (i = 0; i < helpbuttons.length; i++) {
+      var openHelp = helpbuttons[i];
+      if (openHelp.classList.contains('show')) {
+        openHelp.classList.remove('show');
+      }
+    }
+  }
+};
